7fdff9c8d0da09103c41b4de1cd97748e7a2bc57
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-04-27 23:32:49 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,8 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => <div key={movie.title}><h3>{movie.title}</h3><h6>{movie.time}</h6><ul>{movie.genres.map(genre => <li key={genre}>{genre}</li>)}</ul></div>)}
     </div>
   );
 };
